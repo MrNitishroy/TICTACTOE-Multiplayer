17dbca20787504c0eee2b2f6519f6abe3bf8095f
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-03-29 10:59:05 +0530

    Assets added

--- lib/Configs/AssetsPath.dart
+++ lib/Configs/AssetsPath.dart
@@ -7,4 +7,24 @@ class IconsPath {
   static const smsIcon = "${basePath}sms.svg";
   static const wonIcon = "${basePath}won.svg";
   static const xIcon = "${basePath}x.svg";
+  static const applogo = "${basePath}applogo.svg";
+  static const camera = "${basePath}camera.svg";
+  static const gallery = "${basePath}gallery.svg";
+  static const game = "${basePath}game.svg";
+  static const github = "${basePath}github.svg";
+  static const google = "${basePath}google.svg";
+  static const group = "${basePath}group.svg";
+  static const info = "${basePath}info.svg";
+  static const save = "${basePath}save.svg";
+  static const user = "${basePath}user.svg";
+  static const youtube = "${basePath}youtube.svg";
+}
+
+class ImagePath {
+  static const basePath = "assets/photos/";
+  static const boy = "${basePath}boy.png";
+  static const girl = "${basePath}girl.png";
+  static const welcome1 = "${basePath}welcome1.png";
+  static const welcome2 = "${basePath}welcome2.png";
+  static const welcome3 = "${basePath}welcome3.png";
 }
